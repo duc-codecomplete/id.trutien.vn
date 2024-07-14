d6efd9444ed8dc719cfa5894382e8156756508de
Duc Nguyen <duc.nguyen@codecomplete.jp>
2024-07-14 15:51:11 +0700

    Update payment

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -46,7 +46,7 @@ class ApiController extends Controller
             $user->balance = $amount_promotion;
             $trans->save();
             $user->save();
-            $msg = "Người chơi " . $username . "đã nạp " . number_format($amount) . "";
+            $msg = "Người chơi " . $username . " đã nạp " . number_format($amount) . "";
             $this->sendMessage($msg);
 
             return response()->json("ok", 200);
